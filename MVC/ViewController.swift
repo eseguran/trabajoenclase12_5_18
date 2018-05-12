79bd8fd3f34544a9ec5451f8d8bce9a6425bc4f8
Eddy Segura <eseguran@outlook.com>
2018-05-12 13:39:37 -0600

    Initial Commit

---
+++ MVC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MVC
+//
+//  Created by Eddy Segura on 5/12/18.
+//  Copyright © 2018 Eddy Segura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
